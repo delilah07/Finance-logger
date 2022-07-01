b2f4b60d5d7c1e73f3af6ed43efc088ea411172a
unknown <you@example.com>
2022-07-01 17:16:13 +0200

    feat: add creattion of li in html

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/invoices.js";
+import { ListTemplate } from "./classes/listTemplate.js";
 import { Payment } from "./classes/payment.js";
 // -------------- form ----------------
 // const form = document.querySelector('form')!;
@@ -9,16 +10,21 @@ var type = document.querySelector("#type");
 var toFrom = document.querySelector("#tofrom");
 var details = document.querySelector("#details");
 var amount = document.querySelector("#amount");
+// list template instance
+var ul = document.querySelector("ul");
+var list = new ListTemplate(ul);
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     var doc;
     if (type.value === "invoice") {
         doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        console.log(doc);
     }
     else {
         doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        console.log(doc);
     }
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    console.log(list.render(doc, type.value, "end"));
 });
 //
 //
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,5 @@
 import { Invoice } from "./classes/invoices.js";
+import { ListTemplate } from "./classes/listTemplate.js";
 import { Payment } from "./classes/payment.js";
 import { HasFormatter } from "./interfaces/HasForutter";
 
@@ -13,16 +14,23 @@ const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
   let doc: HasFormatter;
   if (type.value === "invoice") {
     doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    console.log(doc);
   } else {
     doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    console.log(doc);
   }
-  console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+
+  console.log(list.render(doc, type.value, "end"));
 });
 
 //
